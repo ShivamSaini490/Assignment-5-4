3956778d75382a9eaedf6f6916dc5e3c3a93cfe7
ShivamSaini490 <126521640+ShivamSaini490@users.noreply.github.com>
2023-12-04 14:56:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class CustomException : Exception
+{
+    public CustomException(string message) : base(message)
+    {
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int value = 58;
+
+            if (value == 58)
+            {
+                throw new CustomException("Custom Exception: Value cannot be 58.");
+            }
+
+            Console.WriteLine("Value is not 58. Program continues...");
+        }
+        catch (CustomException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
